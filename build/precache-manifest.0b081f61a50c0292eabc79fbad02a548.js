4a4acaff46978159425ced4efd2b27bd71068d1f
Pushpak Sharma <pushpaksharma@Pushpaks-Air.ht.home>
2023-02-09 11:31:49 +0530

    first commit

---
+++ build/precache-manifest.0b081f61a50c0292eabc79fbad02a548.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26273162463a3785b09fa9168a99447f",
+    "url": "/puppy-age/index.html"
+  },
+  {
+    "revision": "0825a3688452a4438fb3",
+    "url": "/puppy-age/static/css/main.81e6fef5.chunk.css"
+  },
+  {
+    "revision": "b57c39673f650a7a3894",
+    "url": "/puppy-age/static/js/2.6f4f9af9.chunk.js"
+  },
+  {
+    "revision": "0825a3688452a4438fb3",
+    "url": "/puppy-age/static/js/main.1f5f18a8.chunk.js"
+  },
+  {
+    "revision": "985e1926b0c155342f44",
+    "url": "/puppy-age/static/js/runtime~main.6c8f6d6d.js"
+  }
+]);
